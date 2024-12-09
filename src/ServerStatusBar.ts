f4a82e3bccf0f31c5cc36444ae26910e3cedd4b6
scott carver <scott@artificia.org>
2024-12-09 18:01:51 +0100

    Improve and modularize status display

--- src/ServerStatusBar.ts
+++ src/ServerStatusBar.ts
@@ -20,7 +20,8 @@ export class ServerStatusBar {
       vscode.StatusBarAlignment.Right,
       100
     );
-    this.statusBarItem.name = "Supercollider Server Status";
+    this.statusBarItem.name = "SuperCollider server status";
+    this.statusBarItem.color = new vscode.ThemeColor("disabledForeground");
     this.statusBarItem.show();
   }
 
@@ -28,8 +29,22 @@ export class ServerStatusBar {
     return this.statusBarItem;
   }
 
+  formatString(string: string, name, icon, avgCPUFormatted, peakCPUFormatted, numUGens, numSynths, numGroups, numSynthDefs): string {
+    string = string.replace("${name}", name)
+    string = string.replace("${icon}", icon)
+    string = string.replace("${avgCPU}", avgCPUFormatted);
+    string = string.replace("${peakCPU}", peakCPUFormatted);
+    string = string.replace("${numUGens}", numUGens.toString());
+    string = string.replace("${numSynths}", numSynths.toString());
+    string = string.replace("${numGroups}", numGroups.toString());
+    string = string.replace("${numSynthDefs}", numSynthDefs.toString());
+
+    return string
+  }
+
   updateStatusBar(data: ServerStatusBarData) {
     let {
+      name,
       running,
       unresponsive,
       avgCPU,
@@ -39,6 +54,7 @@ export class ServerStatusBar {
       numGroups,
       numSynthDefs,
     } = data;
+
     if (!running) {
       unresponsive = false;
       avgCPU = 0;
@@ -56,25 +72,50 @@ export class ServerStatusBar {
     const icon = unresponsive
       ? `$(warning)`
       : running
-      ? `$(play)`
-      : `$(primitive-square)`;
+        ? `$(play)`
+        : `$(primitive-square)`;
+
+    this.statusBarItem.color = unresponsive
+      ? new vscode.ThemeColor("activityWarningBadge.foreground")
+      : running
+        ? new vscode.ThemeColor("terminal.ansiGreen")
+        : new vscode.ThemeColor("disabledForeground");
 
     this.statusBarItem.command = unresponsive
       ? "supercollider.internal.rebootServer"
       : running
-      ? "supercollider.internal.killAllServers"
-      : "supercollider.internal.bootServer";
+        ? null
+        : "supercollider.internal.bootServer";
 
     this.statusBarItem.tooltip = unresponsive
       ? "Server is unresponsive. Click to restart."
       : running
-      ? "Server is running. Click to stop."
-      : "Server is not running. Click to start.";
+        ? "Server is running."
+        : "Server is not running. Click to start.";
 
     const avgCPUFormatted = percentFormatter.format(avgCPU);
-
     const peakCPUFormatted = percentFormatter.format(peakCPU);
 
-    this.statusBarItem.text = `${icon} ${avgCPUFormatted}% ${peakCPUFormatted}% ${numUGens}u ${numSynths}s ${numGroups}g ${numSynthDefs}d`;
+    const configuration = vscode.workspace.getConfiguration()
+
+    const statusString = this.formatString(
+      configuration.get<string>('supercollider.serverStatusString'),
+      name, icon, avgCPUFormatted, peakCPUFormatted, numUGens, numSynths, numGroups, numSynthDefs);
+
+    const tooltipString = this.formatString(
+      "**Status**: " + (running ? "`running`" : "`stopped`") + "  \n" +
+      "---\n" +
+      "**Average CPU**: `${avgCPU}%`  \n" +
+      "**Peak CPU**: `${peakCPU}%`  \n" +
+      "---\n" +
+      "**Synths**: `${numSynths}`  \n" +
+      "**Groups**: `${numGroups}`  \n" +
+      "**UGens**: `${numUGens}`  \n" +
+      "---\n" +
+      "**SynthDefs**: `${numSynthDefs}`  \n",
+      name, icon, avgCPUFormatted, peakCPUFormatted, numUGens, numSynths, numGroups, numSynthDefs);
+
+    this.statusBarItem.text = statusString;
+    this.statusBarItem.tooltip = new vscode.MarkdownString(tooltipString);
   }
 }
